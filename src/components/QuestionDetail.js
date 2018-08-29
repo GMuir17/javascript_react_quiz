9c1ad25778e4aea4a9e4f356b93437597bb212f9
Gary Muir <garymuir17@gmail.com>
2018-08-29 10:56:55 +0100

    Question Detail class started

---
+++ src/components/QuestionDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuestionDetail = function (props) {
+  if(!props.question) return null;
+
+  return (
+    
+  )
+}
+
+export default QuestionDetail;
---
+++ src/containers/QuestionContainer.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import QuestionDetail from '../components/QuestionDetail.js';
+import AnswerSubmit from '../components/AnswerSubmit.js';
+import AnswerOptions from '../components/AnswerOptions.js';
+import Result from '../components/Result.js';
+
+class QuestionContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: null,
+      answers: [],
+      correctAnswer: null
+    };
+  };
+
+  componentDidMount() {
+    const url = 'https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple';
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const incorrectAnswers = data.results[0].incorrect_answers;
+        const rightAnswer = data.results[0].correct_answer;
+        const allAnswers = incorrectAnswers.push(rightAnswer);
+
+        this.setState({
+          question: data.results[0].question,
+          answers: incorrectAnswers,
+          correctAnswer: data.results[0].correct_answer
+        });
+
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className ="question-container">
+       <QuestionDetail
+         question= {this.state.question}
+         />
+      <AnswerOptions
+        answers ={this.state.answers}
+      />
+
+      <AnswerSubmit
+        correctAnswer = {this.state.correctAnswer}
+      />
+
+      <Result
+      />
+
+      </div>
+    );
+  }
+
+
+
+
+};
+
+export default QuestionContainer;
